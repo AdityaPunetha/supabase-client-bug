608c002d20fd10b92bc192338a5a4a6f8586a276
Andrew Smith <a.smith@silentworks.co.uk>
2024-01-14 11:02:18 +0000

    update to use async client and call it as a dependency

--- auth.py
+++ auth.py
@@ -1,18 +1,25 @@
 from fastapi import APIRouter, Depends
-from supabase import Client, create_client
-
-from config import settings
 from oauth2 import get_current_user
+from supabase_client import Client, create_supabase
 
 router = APIRouter(prefix="/auth", tags=["Authentication"])
-supabase: Client = create_client(settings.supabase_url, settings.supabase_key)
 
 
 @router.post("/google")
-def google():
-    data = supabase.auth.sign_in_with_oauth(
+async def google(supabase: Client = Depends(create_supabase)):
+    data = await supabase.auth.sign_in_with_oauth(
         {
             "provider": "google",
+        }
+    )
+    return data
+
+
+@router.post("/github")
+async def github(supabase: Client = Depends(create_supabase)):
+    data = await supabase.auth.sign_in_with_oauth(
+        {
+            "provider": "github",
         }
     )
     return data
@@ -21,3 +28,8 @@ def google():
 @router.get("/protected")
 async def protected_route(session: dict = Depends(get_current_user)):
     return {"message": "You are authorized to access this route"}
+
+
+@router.post("/signout")
+async def sign_out(supabase: Client = Depends(create_supabase)):
+    await supabase.auth.sign_out()
